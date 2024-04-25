2740015b53335d7aed57abaf903e6fb9bbdf0775
shouryadutt <112843008+shouryadutt@users.noreply.github.com>
2024-04-25 11:27:49 +0530

    Update buildAndPushDockerImage.groovy

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -12,7 +12,7 @@ def call(String imageName, String awsAccountId, String awsRegion) {
                             credentialsId: 'K8s',
                             secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
                         ]]) {
-                            bat 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 891376912626.dkr.ecr.us-east-1.amazonaws.com'
+                            sh 'aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 810678507647.dkr.ecr.eu-north-1.amazonaws.com'
                         }
                     }
                 }
@@ -20,29 +20,29 @@ def call(String imageName, String awsAccountId, String awsRegion) {
             stage('Build Docker Image') {
                 steps {
                     script {
-                        docker.build('angular-app')
+                        docker.build('angular-build')
                     }
                 }
             }
             stage('Push Docker Image to AWS ECR') {
                 steps {
                     script {
-                        docker.withRegistry('https://891376912626.dkr.ecr.us-east-1.amazonaws.com/angular-app') {
-                            docker.image('angular-app').push('latest')
+                        docker.withRegistry('https://810678507647.dkr.ecr.eu-north-1.amazonaws.com/angular-build:latest') {
+                            docker.image('angular-build').push('latest')
                         }
                     }
                 }
             }
-            stage('Pull and Run docker image') {
-                steps {
-                    script {
-                        bat 'docker pull 891376912626.dkr.ecr.us-east-1.amazonaws.com/angular-app:latest'
-                        bat 'docker run -d -p 4200:4200 891376912626.dkr.ecr.us-east-1.amazonaws.com/angular-app:latest'
-                        bat 'docker ps'
-                        bat 'docker ps -a'
-                    }
-                }
-            }
+            // stage('Pull and Run docker image') {
+            //     steps {
+            //         script {
+            //             bat 'docker pull 891376912626.dkr.ecr.us-east-1.amazonaws.com/angular-app:latest'
+            //             bat 'docker run -d -p 4200:4200 891376912626.dkr.ecr.us-east-1.amazonaws.com/angular-app:latest'
+            //             bat 'docker ps'
+            //             bat 'docker ps -a'
+            //         }
+            //     }
+            // }
         }
     }
 }
