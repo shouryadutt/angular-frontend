a65048f0d15adcab65c0be56cb5b5bc99c21aa7a
shouryadutt <112843008+shouryadutt@users.noreply.github.com>
2024-04-28 16:22:33 +0530

    Update run.groovy

--- vars/run.groovy
+++ vars/run.groovy
@@ -1,1 +1,49 @@
 
+def call(String imageName, String awsAccountId, String awsRegion) {
+    pipeline {
+        agent any
+      
+        stages {
+            stage('Authenticate with AWS ECR') {
+                steps {
+                    script {
+                        withCredentials([[
+                            $class: 'AmazonWebServicesCredentialsBinding',
+                            accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                            credentialsId: 'K8s',
+                            secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                        ]]) {
+                            bat 'aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 810678507647.dkr.ecr.eu-north-1.amazonaws.com'
+                        }
+                    }
+                }
+            }
+            stage('Build Docker Image') {
+                steps {
+                    script {
+                        docker.build('angular-build')
+                    }
+                }
+            }
+            stage('Push Docker Image to AWS ECR') {
+                steps {
+                    script {
+                        docker.withRegistry('https://810678507647.dkr.ecr.eu-north-1.amazonaws.com/angular-build:latest') {
+                            docker.image('angular-build').push('latest')
+                        }
+                    }
+                }
+            }
+            // stage('Pull and Run docker image') {
+            //     steps {
+            //         script {
+            //             bat 'docker pull 891376912626.dkr.ecr.us-east-1.amazonaws.com/angular-app:latest'
+            //             bat 'docker run -d -p 4200:4200 891376912626.dkr.ecr.us-east-1.amazonaws.com/angular-app:latest'
+            //             bat 'docker ps'
+            //             bat 'docker ps -a'
+            //         }
+            //     }
+            // }
+        }
+    }
+}
